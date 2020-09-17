9bdfc93b0551c6dc5d5d16c1837380b295735cb5
Jocelino Neto <netojocelino@inventione.com.br>
2020-09-17 13:34:20 -0300

    Comandos movidos para handlers

--- cli/handler.go
+++ cli/handler.go
@@ -4,7 +4,59 @@ import (
 	"fmt"
 
 	"github.com/netojocelino/spotinow/authenticate"
+	"github.com/zmb3/spotify"
 )
+
+// CommandHandler handler a command from the CLI
+type CommandHandler interface {
+	HandleCommand()
+}
+
+// PlayingNow show whats playing in current moment
+type PlayingNow struct {
+	client *spotify.Client
+}
+
+// UserProfile show current user data
+type UserProfile struct {
+	client *spotify.Client
+}
+
+// HandleCommand make something
+func (h *PlayingNow) HandleCommand() {
+	currentlyPlaying, currentlyPlayingError := h.client.PlayerCurrentlyPlaying()
+	if (currentlyPlayingError != nil) || (currentlyPlaying.Item == nil) {
+		fmt.Printf("%s\n", "Não foi identificado nenhuma música sendo reproduzida.")
+		return
+	}
+
+	artistsName := "- "
+
+	artistsList := currentlyPlaying.Item.Artists
+	artistsListLen := len(artistsList)
+
+	for indexArtist, artist := range artistsList {
+		artistsName += artist.Name
+		if indexArtist < (artistsListLen - 2) {
+			artistsName += ", "
+		} else if indexArtist < (artistsListLen - 1) {
+			artistsName += " e "
+		} else {
+			artistsName += "."
+		}
+	}
+	fmt.Printf("Tocando: %s %s \n", currentlyPlaying.Item.Name, artistsName)
+}
+
+// HandleCommand make something
+func (h *UserProfile) HandleCommand() {
+	user, userErr := h.client.CurrentUser()
+	if userErr != nil {
+		fmt.Println(userErr)
+		return
+	}
+	fmt.Printf("%s <%s> [%s]\n", user.DisplayName, user.Product, user.ID)
+}
 
 // HandlerCommandLineInput handler with command and do magic
 func HandlerCommandLineInput(command string) {
@@ -13,36 +65,16 @@ func HandlerCommandLineInput(command string) {
 
 	switch command {
 	case "now":
-		currentlyPlaying, currentlyPlayingError := client.PlayerCurrentlyPlaying()
-		if (currentlyPlayingError != nil) || (currentlyPlaying.Item == nil) {
-			fmt.Printf("%s\n", "Não foi identificado nenhuma música sendo reproduzida.")
-			return
+		playingNow := PlayingNow{
+			client: client,
 		}
-
-		artistsName := "- "
-
-		artistsList := currentlyPlaying.Item.Artists
-		artistsListLen := len(artistsList)
-
-		for indexArtist, artist := range artistsList {
-			artistsName += artist.Name
-			if indexArtist < (artistsListLen - 2) {
-				artistsName += ", "
-			} else if indexArtist < (artistsListLen - 1) {
-				artistsName += " e "
-			} else {
-				artistsName += "."
-			}
-		}
-		fmt.Printf("Tocando: %s %s \n", currentlyPlaying.Item.Name, artistsName)
+		playingNow.HandleCommand()
 
 	case "user":
-		user, userErr := client.CurrentUser()
-		if userErr != nil {
-			fmt.Println(userErr)
-			return
+		currentUser := UserProfile{
+			client: client,
 		}
-		fmt.Printf("%s <%s> [%s]\n", user.DisplayName, user.Product, user.ID)
+		currentUser.HandleCommand()
 	default:
 		fmt.Println("Comando não implementado")
 	}
